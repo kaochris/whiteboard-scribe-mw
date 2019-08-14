aee463a701baa2a4e952f3c65618842cb703ed56
amcgaugh <11729100+amcgaugh@users.noreply.github.com>
2019-08-14 09:38:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
+
+
+const fileName = 'https://freepngimg.com/thumb/paper_sheet/48526-3-calligraphy-free-hq-image.png';
+
+// Read a remote image as a text document
+const [result] = await client.documentTextDetection(
+  `gs://${fileName}`
+);
+const fullTextAnnotation = result.fullTextAnnotation;
+console.log(fullTextAnnotation.text);
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
